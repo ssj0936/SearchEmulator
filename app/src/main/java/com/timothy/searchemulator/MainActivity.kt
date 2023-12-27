497926feda4eb5ce51793d487da58328d77f463f
ssj0936 <tim77813@gmail.com>
2023-12-27 19:18:40 +0800

    debugging

--- app/src/main/java/com/timothy/searchemulator/MainActivity.kt
+++ app/src/main/java/com/timothy/searchemulator/MainActivity.kt
@@ -6,25 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.timothy.searchemulator.ui.emulator.EmulatorPage
 import com.timothy.searchemulator.ui.theme.SearchEmulatorTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SearchEmulatorTheme {
-                val windowSize:WindowSizeClass = calculateWindowSizeClass(this)
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -36,19 +28,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-//
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    SearchEmulatorTheme {
-//        Greeting("Android")
-//    }
-//}
