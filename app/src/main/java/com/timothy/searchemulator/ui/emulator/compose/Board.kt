08e66e2bed6098581eb6fbf2ecbf287005c8bea2
ssj0936 <tim77813@gmail.com>
2024-01-10 19:14:25 +0800

    snack bar for searching result

--- app/src/main/java/com/timothy/searchemulator/ui/emulator/compose/Board.kt
+++ app/src/main/java/com/timothy/searchemulator/ui/emulator/compose/Board.kt
@@ -11,17 +11,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.timothy.searchemulator.ui.emulator.Contract
 import com.timothy.searchemulator.ui.emulator.EmulatorViewModel
 import com.timothy.searchemulator.ui.theme.SearchEmulatorTheme
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import androidx.compose.runtime.rememberCoroutineScope as rememberCoroutineScope
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,10 +37,28 @@ fun EmulatorPage(
     viewModel: EmulatorViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsState()
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    Timber.d("state:$state")
+    LaunchedEffect(key1 =Unit){
+        viewModel.effect.collect{effect->
+            when(effect){
+                is Contract.Effect.OnSearchFinish ->{
+                    val isSuccess = effect.isSuccess
+                    scope.launch {
+                        snackbarHostState.showSnackbar(if(isSuccess) "found path" else "path not found")
+                    }
+                }
+            }
+        }
+    }
+//    Timber.d("state:$state")
 
-    Scaffold { paddingValues ->
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { paddingValues ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
